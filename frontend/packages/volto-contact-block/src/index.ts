9b493718b84bb362270b64d7f4b1cea937234424
iFlameing <ialokkumarsingh0@gmail.com>
2025-11-14 20:31:36 +0530

    update to latest volto and setup

--- frontend/packages/volto-contact-block/src/index.ts
+++ frontend/packages/volto-contact-block/src/index.ts
@@ -1,10 +1,20 @@
 import type { ConfigType } from '@plone/registry';
+import type { BlockConfigBase } from '@plone/types';
 import installSettings from './config/settings';
-import installReducer from './config/reducers'
+import installReducer from './config/reducers';
+import blockConfig from './components';
+import './theme/contactForm-base.css';
+
+declare module '@plone/types' {
+  export interface BlocksConfigData {
+    contactList: BlockConfigBase;
+  }
+}
 
 function applyConfig(config: ConfigType) {
   installSettings(config);
   installReducer(config);
+  config.blocks.blocksConfig.contactList = blockConfig;
 
   return config;
 }
