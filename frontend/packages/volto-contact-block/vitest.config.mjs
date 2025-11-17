4ee751edeb9bf07a60cfe592338c58683dec6e94
iFlameing <ialokkumarsingh0@gmail.com>
2025-11-17 15:31:31 +0530

    fix the unit tet in correct way

--- frontend/packages/volto-contact-block/vitest.config.mjs
+++ frontend/packages/volto-contact-block/vitest.config.mjs
@@ -4,10 +4,6 @@ import path from 'path';
 
 export default defineConfig({
   ...voltoVitestConfig,
-  test: {
-    ...voltoVitestConfig.test,
-    passWithNoTests: true,
-  },
   resolve: {
     alias: {
       '@plone/volto': path.resolve(__dirname, '../../core/packages/volto/src'),
