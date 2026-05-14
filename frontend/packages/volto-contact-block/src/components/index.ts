186177a90253674614ae2202d9584ae1e452c484
iRohitSingh <isinghrohit0@gmail.com>
2026-05-14 19:34:40 +0530

    Add german translations and fix css

--- frontend/packages/volto-contact-block/src/components/index.ts
+++ frontend/packages/volto-contact-block/src/components/index.ts
@@ -6,7 +6,7 @@ import ContactListEdit from '../components/Edit';
 
 const blockConfig: BlockConfigBase = {
   id: 'contactList',
-  title: 'Contact list',
+  title: 'Contacts List',
   icon: emailSVG,
   group: 'common',
   view: ContactListView,
